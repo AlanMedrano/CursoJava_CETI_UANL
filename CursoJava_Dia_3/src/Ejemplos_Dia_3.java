044923256edd4cbe7484f8c201d7970e663b6c5e
AlanMedrano <aemedranom@gmail.com>
2019-11-16 14:32:24 -0600

    Primer carga de los ejercicios creados en el Curso de Java Basico - Intermedio.

---
+++ CursoJava_Dia_3/src/Ejemplos_Dia_3.java
@@ -0,0 +1,13 @@
+
+public class Ejemplos_Dia_3 {
+	
+    public static void main(String[] parametro) {
+    //		
+    //	TODO EL CODIGO DE EJECUCION VA EN ESTA PARTE
+    	System.out.println("Hola Mundo Java");
+    
+
+
+    }
+	
+}
